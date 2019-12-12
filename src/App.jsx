cd2b52f403beaca034c7b4225389bc27d06b7e98
justin <justinkim0310@gmail.com>
2019-12-12 12:28:42 +1000

    created useLocation custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import useLocation from "./useLocation";
 
 const App = () => {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      position => setLat(position.coords.latitude),
-      error => setErrorMessage(error.message)
-    );
-  }, []);
+  const [lat, errorMessage] = useLocation();
 
   let content;
 
